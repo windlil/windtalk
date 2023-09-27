62cea5a248caed28396a31edd089614ba31202ab
windlil <windlil@outlook.com>
2023-09-27 16:39:25 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from '@/layout/index.vue'
+import SiderBar from '@/components/SiderBar/index.vue'
+import Right from '@/components/Right/index.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <Layout>
+      <template #left>
+        <SiderBar />
+      </template>
+      <template #center>
+        <RouterView />
+      </template>
+      <template #right>
+        <Right />
+      </template>
+    </Layout>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="less">
+.app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3f3f3f;
 }
 </style>
