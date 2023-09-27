6d096dcba7243c6ecb03e716d17ce0ba78937e76
windlil <windlil@outlook.com>
2023-09-27 16:38:40 +0800

    chore: add layout

---
+++ src/layout/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="layout">
+    <div class="left">
+      <slot name="left" />
+    </div>
+    <div class="center">
+      <slot name="center" />
+    </div>
+    <div class="right">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.layout {
+  display: flex;
+  width: 90%;
+  height: 95%;
+  border-radius: 15px;
+  background-color: #0f0f0f;
+  box-shadow: 0 0 5px 2px #2a2a2a;
+
+  .left {
+    height: 100%;
+    width: 250px;
+    border-right: 1px solid #2e2e2e;
+  }
+
+  .center {
+    box-sizing: border-box;
+    height: 100%;
+    flex: 1;
+    padding: 20px;
+  }
+
+  .right {
+    box-sizing: border-box;
+    height: 100%;
+    width: 350px;
+    padding: 20px;
+  }
+}
+</style>
