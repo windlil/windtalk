6b9d8c5501a70a27cd60603004c2683e0f703eb8
windlil <windlil@outlook.com>
2023-09-27 16:38:57 +0800

    chore: init pages

---
+++ src/pages/main/Home/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HomeContainer from '@/components/HomeContainer/index.vue'
+</script>
+
+<template>
+  <div class="home">
+    <HomeContainer />
+  </div>
+</template>
+
+<style scoped lang="less">
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
